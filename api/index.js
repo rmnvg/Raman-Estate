38cb6065a799ec71be152d49d67abfb51e772300
rmnvg <ramanjotsingh247@gmail.com>
2024-07-02 01:26:52 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose'; 
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(
+    () => {
+        console.log('Connected to MongoDB!');
+
+    }).catch((err) => {
+        console.log(err);
+    }
+    );
 
 const app = express();
 
-app.listen(3500, () => {
+app.listen(3600, () => {
     
     console.log('Server is running on port 3500')
 });
